2e15b086be3183e6fea07d9351beac29cde2f96f
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-10-11 21:31:44 -0400

    Guess functionality completed

--- src/app/components/hangman-display/hangman-display.component.ts
+++ src/app/components/hangman-display/hangman-display.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-hangman-display',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './hangman-display.component.html',
   styleUrl: './hangman-display.component.scss'
 })
@@ -14,26 +17,53 @@ export class HangmanDisplayComponent implements OnChanges {
 
   Max_Mistakes = 7;
   mistakesRemaining !: number;
+  success: boolean = false;
 
   constructor() {
     this.mistakesRemaining = this.Max_Mistakes;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const guessesCurrentValue = changes['guesses'].currentValue;
+    const guessesCurrentValue = changes?.['guesses']?.currentValue;
+
+    // Reset counter for new question
+    if(guessesCurrentValue && guessesCurrentValue != changes?.['guesses']?.previousValue) {
+      this.mistakesRemaining = this.Max_Mistakes;
+      this.success = false;
+    }
 
     if(guessesCurrentValue && 
       guessesCurrentValue.length > 0 && 
-      guessesCurrentValue != changes['guesses'].previousValue) 
+      guessesCurrentValue != changes?.['guesses']?.previousValue) 
     {
-      this.checkGuess(guessesCurrentValue[
-        guessesCurrentValue.length - 1
-      ]);
+      const char = [...guessesCurrentValue].pop();
+      this.checkGuess(char);
     }
   }
 
   checkGuess(letter: string) {
-    console.log(letter);
+    let isWinner = true;
+    this.mistakesRemaining -= this.wasGuessAMistake(letter);
+    console.log('Guess: '+ letter + ' and Mistakes remaining: ' + this.mistakesRemaining);
+
+    for(let i = 0; i < this.question.length; i++) {
+      if(!this.guesses.find(
+        (g) => g.toLowerCase() === this.question[i].toLowerCase())) 
+      {
+        isWinner = false;
+        break;
+      }
+    }
+
+    this.success = isWinner;
+    if(this.success || this.mistakesRemaining === 0) {
+      console.log('Game Ended');
+    }
+  }
+
+  wasGuessAMistake(letter: string) {
+    const match = this.question.match(new RegExp(letter, 'gi'));  // Flag='gi' => Global & Case-Insensitive
+    return match ? 0 : 1;
   }
 
 }
--- src/app/components/hangman-question/hangman-question.component.ts
+++ src/app/components/hangman-question/hangman-question.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-hangman-question',
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
   templateUrl: './hangman-question.component.html',
   styleUrl: './hangman-question.component.scss'
 })
-export class HangmanQuestionComponent {
+export class HangmanQuestionComponent implements OnChanges {
+
+  @Input() question: string = '';
+  @Input() guesses: string[] = [];
+  characters: {value: string, guessed: boolean}[] = [];
+
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const guessesCurrentValue = changes?.['guesses']?.currentValue;
+
+    // Reset counter for new question
+    if(guessesCurrentValue && guessesCurrentValue != changes?.['guesses']?.previousValue) {
+      this.characters = this.question
+        .split('')
+        .map((char) => ({ value: char, guessed: false }));
+    }
+
+    if(guessesCurrentValue && 
+      guessesCurrentValue.length > 0 && 
+      guessesCurrentValue != changes?.['guesses']?.previousValue) 
+    {
+      const guessedChar = [...changes?.['guesses']?.currentValue].pop();
+
+      this.characters = this.characters.map(char => {
+        if(char.value.toLowerCase() === guessedChar.toLowerCase()) {
+          return { ...char, guessed: true};
+        }
+        return char;
+      });
+    }
+  }
 
 }
